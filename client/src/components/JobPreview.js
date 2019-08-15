df3cdbf2851b3c6d4aa12db0c03e9ba932f4468a
kevinyjiang <kyj2108@columbia.edu>
2019-08-15 14:57:16 -0400

    add buttons

--- client/src/components/JobPreview.js
+++ client/src/components/JobPreview.js
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 
 export default class JobPreview extends Component {
 
+  apply() {
+    
+  }
+
   render() {
     return (
       <div className="jobPreview">
-        <ul>
+        <ul className="jobDetails">
           <li><strong>Name:</strong> {this.props.job.name}</li>
           <li><strong>Date:</strong> {this.props.job.date}</li>
           <li><strong>Time:</strong> {this.props.job.time}</li>
           <li><strong>Location:</strong> {this.props.job.location}</li>
           <li><strong>Compensation:</strong> {this.props.job.compensation}</li>
         </ul>
+        <div className="jobOptions">
+          <button onClick={this.apply}>Apply</button>
+          <button>More Info</button>
+        </div>
       </div>
     )
   }
